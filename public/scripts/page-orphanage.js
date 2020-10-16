896f9e2c64291b83dad1977f883306cf16f391ec
lucas <lucasmartins0498@gmail.com>
2020-10-16 10:39:49 -0300

    Lesson 02 completed

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,49 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
+// Create map
+const map = L.map('mapid', options).setView([-26.2500217,-49.5185779], 15)
+
+// Create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+
+//Create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+// Create and add marker
+L.marker([-26.2500217,-49.5185779], { icon })
+.addTo(map)
+
+// image gallery
+
+function selectImage(event) {
+    const button = event.currentTarget
+
+    // Remover todas as classes .active
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button) {
+        button.classList.remove("active")
+    }
+
+    // Selecionar a imagem clicada
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+    // Atualizar o container de image
+    imageContainer.src = image.src
+
+    // Adicionar a classe .active para este botao
+    button.classList.add("active")
+}
